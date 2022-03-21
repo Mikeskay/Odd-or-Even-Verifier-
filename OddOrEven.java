9a13086093753599a70b5ccc9a4fc5733ca49823
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 17:08:56 +0000

    Create OddOrEven.java

---
+++ OddOrEven.java
@@ -0,0 +1,24 @@
+package oddoreven.base;
+
+public class OddOrEven {
+
+    public OddOrEven(){
+        
+    }
+
+    public static void printOddOrEven(int num){
+
+        if(num % 2 == 0){
+            System.out.println("Number entered is an even integer value.");
+        }
+
+        else {
+            System.out.println("Number entered is an odd integer value.");
+        }
+
+
+    }
+
+    
+
+}
